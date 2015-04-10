ee36f9433e4d990746615ebb2b864089de505456
Michael Porokhovnichenko <marazmiki@gmail.com>
2015-04-10 11:52:51 +0600

    drop django-classy-tags

--- django_comments_threaded/templatetags/threaded_comments.py
+++ django_comments_threaded/templatetags/threaded_comments.py
@@ -6,41 +6,14 @@ from __future__ import absolute_import
 from __future__ import division
 from django import template
 # from classytags.core import Tag, Options
-from classytags.core import Options
-from classytags.arguments import Argument, KeywordArgument
-from classytags.helpers import InclusionTag
+# from classytags.core import Options
+# from classytags.arguments import Argument, KeywordArgument
+# from classytags.helpers import InclusionTag, AsTag
 
 
 register = template.Library()
 
 
-class InsertThreadedComments(InclusionTag):
-    name = 'insert_comments'
-    template = 'django_comments_threaded/templatetags/insert_comments.html'
-
-    options = Options(
-        'for',
-        Argument('name', required=False, default='world'),
-        'with',
-        KeywordArgument('anonymous', required=False, default=True),
-        KeywordArgument('template_name', required=False, default=None)
-
-    )
-
-    def get_template(self, context, **kwargs):
-        return kwargs.get('template_name') or self.template
-
-    def get_context(self, context, **kwargs):
-        print('kwargs =', kwargs)
-        return {'varname': 'dummy'}
-
-#    def render_tag(self, context, name, varname):
-#        output = 'hello %s' % name
-#        if varname:
-#            context[varname] = output
-#            return ''
-#        return output
-#
-
-
-register.tag(InsertThreadedComments)
+@register.assignment_tag
+def get_comment_list(content_object):
+    return Comment.objects.get_for_object(content_object)
