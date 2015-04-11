91f5484d799b7c059fb6c88259fd51abc49eabd1
Michael Porokhovnichenko <marazmiki@gmail.com>
2015-04-11 11:53:44 +0600

    new view

--- django_comments_threaded/api/views.py
+++ django_comments_threaded/api/views.py
@@ -4,11 +4,15 @@ from __future__ import unicode_literals
 from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import division
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, CreateAPIView
 from rest_framework.response import Response
 from django_comments_threaded.models import Comment
 from django_comments_threaded.api.serializers import CommentSerializer
 from django_comments_threaded.api.utils import to_tree
+
+
+class CreateView(CreateAPIView):
+    serializer_class = CommentSerializer
 
 
 class ListView(ListAPIView):
