ac589876dca8981fc2e0294622de24316af2b657
Michael Porokhovnichenko <marazmiki@gmail.com>
2015-04-14 01:40:49 +0600

    rest framework in example project

--- example_project/example_project/settings.py
+++ example_project/example_project/settings.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mptt',
+    'rest_framework',
     'django_comments_threaded',
     'generic_helpers',
     'posts',
--- example_project/example_project/urls.py
+++ example_project/example_project/urls.py
@@ -10,5 +10,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^comments/', include('django_comments_threaded.urls')),
     url(r'^', include('posts.urls')),
 ]
