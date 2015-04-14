4fb44735f772af9eb09d2cc51c332192355bdeee
Michael Porokhovnichenko <marazmiki@gmail.com>
2015-04-14 10:46:48 +0600

    Using CommentSerializer in expand_node

--- django_comments_threaded/api/utils.py
+++ django_comments_threaded/api/utils.py
@@ -5,22 +5,17 @@ from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import division
 from mptt.templatetags.mptt_tags import cache_tree_children
+from django_comments_threaded.api.serializers import CommentSerializer
 
-
-def expand_node(node, children_key='replies', exclude=None):
-    result = {k: v for k, v in node.__dict__.items() if not k.startswith('_')}
-
-    if exclude:
-        result = {k: v for k, v in result.items() if k not in exclude}
-
+def expand_node(node, children_key='replies'):
+    result = CommentSerializer(node)
+    data = result.data.copy()
     children = [expand_node(c) for c in node.get_children()]
 
     if children:
-        result[children_key] = children
-    return result
+        data.update(children_key=children)
+    return data
 
 
 def to_tree(queryset):
-    from django_comments_threaded.api.serializers import CommentSerializer
-    ex = CommentSerializer.Meta.write_only_fields
-    return [expand_node(n, exclude=ex) for n in cache_tree_children(queryset)]
+    return [expand_node(n) for n in cache_tree_children(queryset)]
