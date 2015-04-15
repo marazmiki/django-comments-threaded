2a3087a763cd06b5e4efc8582f4f4505bc1ee12e
Michael Porokhovnichenko <marazmiki@gmail.com>
2015-04-15 23:28:38 +0600

    refactor get_version()

--- django_comments_threaded/__init__.py
+++ django_comments_threaded/__init__.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import division
+import sys
 
 
 VERSION = (1, 0, 0)
@@ -11,8 +12,5 @@ default_app_config = 'django_comments_threaded.apps.CommentsThreadedConfig'
 
 
 def get_version():
-    try:
-        text_type = unicode
-    except NameError:
-        text_type = str
+    text_type = str if sys.version_info[0] == 3 else unicode
     return '.'.join(map(text_type, VERSION))
