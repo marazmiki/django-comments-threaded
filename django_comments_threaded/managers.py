d46b575e53a3142b8d227fc1ecbb4fdde3931793
Mikhail Porokhovnichenko <marazmiki@gmail.com>
2015-04-15 20:34:36 +0600

    manager and its tests

--- django_comments_threaded/managers.py
+++ django_comments_threaded/managers.py
@@ -4,11 +4,11 @@ from __future__ import unicode_literals
 from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import division
-from django.db.models import QuerySet
 from mptt.managers import TreeManager
+from generic_helpers.managers import GenericQuerySet
 
 
-class CommentQuerySet(QuerySet, TreeManager):
+class CommentQuerySet(GenericQuerySet, TreeManager):
     def spam(self):
         return self.filter(is_spam=True)
 
---
+++ django_comments_threaded/tests/managers.py
@@ -0,0 +1,47 @@
+# coding: utf-8
+
+from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import division
+from django import test
+from django_comments_threaded.utils import get_model
+from django_comments_threaded.tests import Post
+
+
+Comment = get_model()
+
+
+class TestSpamManagerMethod(test.TestCase):
+    def setUp(self):
+        self.post = Post.objects.create()
+        self.good_comment = Comment.objects.create(content_object=self.post)
+        self.bad_comment = Comment.objects.create(content_object=self.post,
+                                                  is_spam=True)
+
+    def test_count_total(self):
+        self.assertEqual(2, Comment.objects.get_for_object(self.post).count())
+
+    def test_spam(self):
+        self.assertEqual(
+            self.bad_comment,
+            Comment.objects.get_for_object(self.post).spam().get()
+        )
+
+
+class TestInModerationManagerMethod(test.TestCase):
+    def setUp(self):
+        self.post = Post.objects.create()
+        self.good_comment = Comment.objects.create(content_object=self.post,
+                                                   is_moderated=True)
+        self.bad_comment = Comment.objects.create(content_object=self.post,
+                                                  is_moderated=False)
+
+    def test_count_total(self):
+        self.assertEqual(2, Comment.objects.get_for_object(self.post).count())
+
+    def test_in_moderation(self):
+        self.assertEqual(
+            self.bad_comment,
+            Comment.objects.get_for_object(self.post).in_moderation().get()
+        )
