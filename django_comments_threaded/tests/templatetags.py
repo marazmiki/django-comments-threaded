00462908352cd89bd565cde92f983912bb8d7cf1
Michael Porokhovnichenko <marazmiki@gmail.com>
2015-04-19 21:05:40 +0600

    using get_model() and get_create_form() instead of hardcoded

--- django_comments_threaded/tests/templatetags.py
+++ django_comments_threaded/tests/templatetags.py
@@ -6,7 +6,7 @@ from __future__ import absolute_import
 from __future__ import division
 from django import test
 from django.core.urlresolvers import reverse
-from django_comments_threaded.tests import Post, Image
+from django_comments_threaded.tests import Post, Image, create_user
 from django_comments_threaded.templatetags import comments_threaded_tags as t
 from django_comments_threaded.utils import get_model
 
@@ -46,8 +46,17 @@ class TestGetCommentListTag(test.TestCase):
 
 class TestGetCommentFormTag(test.TestCase):
     def setUp(self):
+        self.user = create_user()
         self.post = Post.objects.create()
         self.req = test.RequestFactory()
+
+    def test_get(self):
+        form = t.get_comment_form({'request': self.req.get('/')}, self.post)
+        print(form)
+
+    def test_post(self):                        
+        form = t.get_comment_form({'request': self.req.post('/', {})}, self.post)
+        print(form)
 
 # get_comment_api_urls(content_object, **kwargs):
 # get_comment_list(content_object, **kwargs):
