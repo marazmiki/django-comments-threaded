580425921bbf6fbbe9d52dfdcf400e6ec272b1bf
Michael Porokhovnichenko <marazmiki@gmail.com>
2015-04-26 02:25:10 +0600

    fix pep8 bug

--- django_comments_threaded/api/serializers.py
+++ django_comments_threaded/api/serializers.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import division
-from rest_framework import serializers, parsers
+from rest_framework import serializers
 from django.db.models import permalink
 from django_comments_threaded.models import Comment, LastRead
 
